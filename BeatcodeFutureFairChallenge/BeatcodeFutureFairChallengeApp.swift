0782be11f658337332aed8df9ac2d3c7ee538b64
albo95 <albobruno95@gmail.com>
2025-07-06 10:00:16 +0200

    Added project files

---
+++ BeatcodeFutureFairChallenge/BeatcodeFutureFairChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatcodeFutureFairChallengeApp.swift
+//  BeatcodeFutureFairChallenge
+//
+//  Created by Alberto Bruno on 06/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BeatcodeFutureFairChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
