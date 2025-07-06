af2cac6a29b90dad543ff7cfeca7b3f92a5ac365
albo95 <albobruno95@gmail.com>
2025-07-06 11:12:51 +0200

    Added color palette

---
+++ BeatcodeFutureFairChallenge/Utilities/ColorExtension.swift
@@ -0,0 +1,12 @@
+//
+//  ColorExtension.swift
+//  BeatcodeFutureFairChallenge
+//
+//  Created by Alberto Bruno on 06/07/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let itemPalette: [Color] = [.red, .green, .blue, .orange, .yellow, .mint, .teal, .purple, .pink, .cyan]
+}
